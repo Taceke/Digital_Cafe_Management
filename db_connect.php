8a253bd762ca17b593bc03252b3a314fd3aad24b
Taj <tajerkemer46031@gmail.com>
2025-10-26 05:13:21 +0300

    connect update

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,9 @@
 <?php
-$servername = "localhost:3308";
-$username = "root";  // Change if using a different user
-$password = "";      // Set your database password if required
-$dbname = "cafeteria_pos";
+// Use environment variables if they exist (Render will provide them)
+$servername = getenv("DB_HOST") ?: "localhost";
+$username   = getenv("DB_USER") ?: "root";
+$password   = getenv("DB_PASS") ?: "";
+$dbname     = getenv("DB_NAME") ?: "cafeteria_pos";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
