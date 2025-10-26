7dc633723e26526e735ff1177d07053201334639
Taj <tajerkemer46031@gmail.com>
2025-10-26 07:02:00 +0300

    for railway

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,11 @@
 <?php
-// Use environment variables if they exist (Render will provide them)
 $servername = getenv("DB_HOST") ?: "localhost";
 $username   = getenv("DB_USER") ?: "root";
 $password   = getenv("DB_PASS") ?: "";
 $dbname     = getenv("DB_NAME") ?: "cafeteria_pos";
+$port       = getenv("DB_PORT") ?: "3308";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
